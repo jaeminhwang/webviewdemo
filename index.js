dba52d2f00a64400027ea1b86b06e974aadb6137
jaeminhwang <jeminad2@gmail.com>
2022-04-05 23:16:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,14 @@
     // react-native에 통신을 요청한다.
     $('#btnCommand').on('click', function() {
         var option = {
-            url: '/command.json'
-        }
+    "vender": "OnTheLive",
+    "type": "Request",
+    "command": "TurnOnCam",
+    "transaction": "2",
+    "data": [
+        { "deviceId": "c780cc3e4b848be1a3b71e95dcd7b646e1c59b8ff234af4a3de957114e98c204"}
+    ]
+}
 
         // "doCommand"는 react-native에서 받는 메서드 이름입니다.
         window.webViewBridge.send('doCommand', option, function(res) {
