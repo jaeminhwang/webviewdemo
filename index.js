f2829fa782993d2c82a85c72a6f1fad6c92e1627
jaeminhwang <jeminad2@gmail.com>
2022-04-05 23:45:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,9 @@
     "type": "Request",
     "command": "TurnOnCam",
     "transaction": "2",
-    "data":
-            { "deviceId": "c780cc3e4b848be1a3b71e95dcd7b646e1c59b8ff234af4a3de957114e98c204"}    
+    "data": [
+        { "deviceId": "c780cc3e4b848be1a3b71e95dcd7b646e1c59b8ff234af4a3de957114e98c204"}
+    ]
 }
 
         // "doCommand"는 react-native에서 받는 메서드 이름입니다.
@@ -32,5 +33,28 @@
             console.error(err)
         })
     })
+
+    // react-native에 통신을 요청한다.
+    $('#btnEnum').on('click', function() {
+        var option = {
+    "vender": "OnTheLive",
+    "type": "Request",
+    "command": "EnumerateDevices",
+    "transaction": "1",
+    "data": ""
+    }
+
+        // "doCommand"는 react-native에서 받는 메서드 이름입니다.
+        window.webViewBridge.send('doEnum', option, function(res) {
+            $('#result3').html(JSON.stringify(res))
+        }, function(err) {
+            console.error(err)
+        })
+    })
+
+
+
+
+
 
 }());
