e7ae540b950a470a6ec26a7c97822619243fa1d8
jaeminhwang <jeminad2@gmail.com>
2022-04-05 22:24:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,18 @@
         })
     })
 
+    // react-native에 통신을 요청한다.
+    $('#btnCommand').on('click', function() {
+        var option = {
+            url: '/command.json'
+        }
+
+        // "doCommand"는 react-native에서 받는 메서드 이름입니다.
+        window.webViewBridge.send('doCommand', option, function(res) {
+            $('#result').html(JSON.stringify(res))
+        }, function(err) {
+            console.error(err)
+        })
+    })
+
 }());
