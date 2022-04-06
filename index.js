4bcd5450677447f52caa9d6cc9d48f6c446f18f0
jaeminhwang <jeminad2@gmail.com>
2022-04-06 10:30:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,10 @@
         { "deviceId": "c780cc3e4b848be1a3b71e95dcd7b646e1c59b8ff234af4a3de957114e98c204"}
     ]
 }
-
+       
         // "doCommand"는 react-native에서 받는 메서드 이름입니다.
-        window.webViewBridge.send('doCommand', option, function(res) {
-            $('#result2').html(JSON.stringify(res))
-        }, function(err) {
-            console.error(err)
-        })
+        window.postMessage(option, '*');
+        console.log(">>>>> 테스트 메시지 전송"
     })
 
     // react-native에 통신을 요청한다.
