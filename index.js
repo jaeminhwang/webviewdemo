9fbbb72cdb2fd4e778f8739a31d8140c9461e7b2
jaeminhwang <jeminad2@gmail.com>
2022-04-06 12:48:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,12 @@
     // react-native에 통신을 요청한다.
     $('#btnCommand').on('click', function() {
         var option = {
-    "vender": "OnTheLive",
-    "type": "Request",
-    "command": "TurnOnCam",
-    "transaction": "2",
-    "data": [
-        { "deviceId": "c780cc3e4b848be1a3b71e95dcd7b646e1c59b8ff234af4a3de957114e98c204"}
-    ]
-}
+            "vender": "OnTheLive",
+            "type": "Respone",
+            "command": "TurnOnCam",
+            "transaction": "2",
+            "data": "Success"
+        }
        
         // "doCommand"는 react-native에서 받는 메서드 이름입니다.
         sendMsgToParent(option);
