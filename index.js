cc383cbb6d6b0b5722b6f5d2bbfcbc6ef3ad1e82
jaeminhwang <jeminad2@gmail.com>
2022-04-06 12:57:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 (function () {
 
     // react-native에 통신을 요청한다.
-    $('#btnSend').on('click', function() {
-        var option = {
-            url: '/movie.json'
-        }
-
-        // "getMovieList"는 react-native에서 받는 메서드 이름입니다.
-        window.webViewBridge.send('getMovieList', option, function(res) {
-            $('#result').html(JSON.stringify(res))
-        }, function(err) {
-            console.error(err)
-        })
+    $('#btnSend').on('click', function() {    
+        console.log(">>>>> RN Webview 통신 테스트");
+        window.ReactNativeWebView.postMessage("Hello, React Native Webview");        
     })
     
     function sendMsgToParent( msg ) {
